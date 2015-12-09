a20c40f3ce2f37c300db887885b36015e59c6372
Thibault <thyb44@gmail.com>
2015-12-09 22:25:48 +0100

    Prêt pour la mise en ligne
    Code html valide, style.css valide, couverture du code css vérifié dans
    style.css, tri fait dans les photos ...

--- js/materialize.js
+++ js/materialize.js
@@ -3483,7 +3483,7 @@ $(document).ready(function(){
 
     $(document).on('click.card', '.card', function (e) {
       if ($(this).find('> .card-reveal').length) {
-        if ($(e.target).is($('.card-reveal .card-title')) || $(e.target).is($('.card-reveal .card-title i'))) {
+        if ($(e.target).is($('.card-reveal .card-title')) || $(e.target).is($('.card-reveal .card-title img'))) {
           // Make Reveal animate down and display none
           $(this).find('.card-reveal').velocity(
             {translateY: 0}, {
@@ -3495,7 +3495,7 @@ $(document).ready(function(){
           );
         }
         else if ($(e.target).is($('.card .activator')) ||
-                 $(e.target).is($('.card .activator i')) ) {
+                 $(e.target).is($('.card .activator img')) ) {
           $(e.target).closest('.card').css('overflow', 'hidden');
           $(this).find('.card-reveal').css({ display: 'block'}).velocity("stop", false).velocity({translateY: '-100%'}, {duration: 300, queue: false, easing: 'easeInOutQuad'});
         }
