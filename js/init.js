a30982c8e2304cf8e598171e45d19aad94bd9ee1
ThibaultOsmont <thyb44@gmail.com>
2016-05-30 19:43:41 +0200

    Revert "Bootstrap version"
    This reverts commit 1c1907644fd0ccba0583a86141967b96b5e6100e.

--- js/init.js
+++ js/init.js
@@ -1,63 +1,44 @@
-$(document).ready(function() {
-	
-	/*
-	* Initialisation des tooltips 
-	*/
-	$('[data-toggle="tooltip"]').tooltip(); 
+$(document).ready(function($){
+  $(function(){
 
-	/*
-	* Pour activer les tooltips Bootstrap
-	*/
-	var scrollActel = (document.documentElement && document.documentElement.scrollTop) || document.body.scrollTop;
-	if(scrollActel >= 1080) {
-		$(".navbar").addClass("navbar-fixed-top");
-		$("#apropos, #experience, #competences, #contact").css("padding-top", "70px");
-		//$("body").css("background-color", "#1C2126");
-	}
+    if( !navigator.userAgent.match(/Android/i) &&
+        !navigator.userAgent.match(/webOS/i) &&
+        !navigator.userAgent.match(/iPhone/i) &&
+        !navigator.userAgent.match(/iPod/i)
+    ) {
+      $('.button-collapse').sideNav();
+      $('.parallax').parallax();
 
-	/*
-	* Pour fixer la navbar en haut de la page
-	*/
-	document.body.onscroll = function() {
-		//Compatibilité IE
-		var scroll = (document.documentElement && document.documentElement.scrollTop) || document.body.scrollTop;
+        var userAgent = navigator.userAgent.split(" ");
+        if(navigator.userAgent.indexOf("Chrome") > 0 && strStartsWith(userAgent[userAgent.length -1], "Safari")) {
+            //Cette condition n'est vérifiée que lorque le navigateur utilisé est chrome et non pas un autre navigateur développé
+            //sur une base de chrome comme vivaldi.
+            //CF. console.log(navigator.userAgent)
+            $("#vertical-container").addClass("vertical-container-chrome");
+        }
+        else {
+            $("#vertical-container").addClass("vertical-container-others");
+        }
 
-		if($(".navbar").hasClass("navbar-fixed-top") && scroll < 1079) {
-			$(".navbar").removeClass("navbar-fixed-top");
-			$("#apropos, #experience, #competences, #contact").css("padding-top", "0");
-			//$("body").css("background-color", "rgb(71, 91, 177)");
-		}
+        $(".left li").on("click", function() {
+            $(".left li").removeClass("active");
+            $(this).addClass("active");
+        });
 
-		if(! $(".navbar").hasClass("navbar-fixed-top") && scroll >= 1079) {
-			$(".navbar").addClass("navbar-fixed-top");
-			$("#apropos, #experience, #competences, #contact").css("padding-top", "70px");
-			//$("body").css("background-color", "#1C2126");
-		}
-	}
+        $('a[href^="#"]').click(function(){
+            var id = $(this).attr("href");
+            var offset = $(id).offset().top;
+            $('html, body').animate({scrollTop: offset}, 'slow');
+            return false;
+        });
+    }
 
-	$('#up, .navbar-brand').click(function(){
-		$('html, body').animate({scrollTop: 0}, 'slow');
-		return false;
-	});
+      else {
+        //$("#parallax-contact").css("display", "none");
+        $("header").addClass("mobile");
+        $("#hautdepage").removeAttr("href");
+        $("#vertical-container").addClass("vertical-container-chrome");
+    }
 
-	$("body").scrollspy({
-		target : "#collapsibleNav",
-		offset : 70
-	});
-
-	$(".navbar-links-spy:not(.dropdown) a").click(function(e) {
-		e.preventDefault();
-
-		// Store hash
-		var hash = this.hash;
-		
-		// Using jQuery's animate() method to add smooth page scroll
-		// The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
-		$('html, body').animate({
-			scrollTop: $(hash).offset().top
-		}, 800, function() {
-			// Add hash (#) to URL when done scrolling (default click behavior)
-			window.location.hash = hash;
-		});
-	});
-})
+  }); // end of document ready
+});
